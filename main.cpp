4ee81bb32a726ff18d7798bdcaef7b00652f83c3
Patryk Kuniecki <s26239@szuflandia.pjwstk.edu.pl>
2021-11-06 15:12:30 +0100

    Dodanie 4 liczby

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 
-int x, y, z;
+int x, y, z, p;
 float srednia;
 
 int main(){
-  std::cout << "Podaj 3 liczby";
+  std::cout << "Podaj 4 liczby";
   std::cin >> x;
   std::cin >> y;
   std::cin >> z;
+  std::cin >> p;
 
-  srednia = (x + y + z)/3;
+  srednia = (x + y + z + p)/4;
 
   std::cout << srednia;
 }
